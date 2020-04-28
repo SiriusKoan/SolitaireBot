cac8df77f60dbc84a05dd496e6ed787a924760a3
SiriusKoan <k.peihsun@gmail.com>
2020-04-28 19:55:59 +0800

    refactoring sql schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,5 @@
-CREATE TABLE IF NOT EXISTS room (
+CREATE TABLE IF NOT EXISTS games (
     id  INTEGER PRIMARY KEY AUTOINCREMENT,
     chat_id INTEGER NOT NULL,
-    word    TEXT    NOT NULL,  -- the last word
-    players TEXT    NOT NULL,  -- split players with ';', like '@aaa;@bbb;@ccc'
-    status  BOOLEAN NOT NULL    DEFAULT FALSE,
     rounds  INTEGER NOT NULL    DEFAULT 0
 );
